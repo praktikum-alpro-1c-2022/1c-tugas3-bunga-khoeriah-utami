cf1be96e2c6548a3276700f9eb5e1ab8b0c676de
Bungakhoeriah <114651565+Bungakhoeriah@users.noreply.github.com>
2022-10-20 15:45:07 +0700

    Tugas Praktikum 3

---
+++ Praktikum3 No.3.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+    // deklarasi variabel
+    float I, V, R;
+    float rumusArus, rumusTegangan, rumusHambatan;
+
+    // input
+    cout << "Masukan nilai arus(I): ";
+    cin >> I;
+
+    cout << "Masukan nilai tegangan(V): ";
+    cin >> V;
+
+    cout << "Masukan nilai hambatan(R): ";
+    cin >> R;
+
+    // rumus
+    rumusArus = V / R;
+    rumusTegangan = I * R;
+    rumusHambatan = V / I;
+
+    // hasil akhir
+    cout << "Hasil perhitungan dari arus(I): " << rumusArus << endl;
+    cout << "Hasil perhitungan dari tegangan(V): " << rumusTegangan << endl;
+    cout << "Hasil perhitungan dari hambatan(R): " << rumusHambatan << endl;
+
+    return 0;
+
+}
